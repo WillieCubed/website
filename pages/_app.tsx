33d06ca9ecfbeff722d630395b51dfcba98fcbbe
Willie Chalmers III <willie@williecubed.me>
2022-05-27 22:56:07 -0700

    chore: Set up new site structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+/**
+ * A wrapper for website styles and additional site-wide client-side code.
+ */
+function WebsiteApp({ Component, pageProps }: AppProps) {
+  return (
+    <div className="min-h-screen dark:bg-slate-900 dark:text-slate-300">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default WebsiteApp;
---
+++ pages/about.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next/types";
+
+const AboutPage: NextPage = () => {
+  return;
+};
+
+export default AboutPage;

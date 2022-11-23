0dac11a23d6815f64250dc6f01d192b65e767e9a
Willie Chalmers III <willie@williecubed.me>
2022-11-23 00:54:27 -0600

    feat: Set up Google Analytics to monitor site

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from 'next/script';
 
 /**
  * A wrapper for website styles and additional site-wide client-side code.
@@ -7,6 +8,16 @@ import type { AppProps } from "next/app";
 function WebsiteApp({ Component, pageProps }: AppProps) {
   return (
     <div className="min-h-screen dark:bg-slate-900 dark:text-slate-300">
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG_ID}`} strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GTAG_ID}');
+        `}
+      </Script>
       <Component {...pageProps} />
     </div>
   );

a75dba5edd2f250d9552b5f5ab2c85a0ab87994f
Willie Chalmers III <willie@williecubed.me>
2022-11-24 01:04:32 -0600

    chore: Update page name for media initiatives

--- pages/media.tsx
+++ pages/media.tsx
@@ -9,14 +9,15 @@ const MediaPage: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Willie Chalmers III - Media and Art</title>
+        <title>Media and Art Initiatives - Willie Chalmers III</title>
         <meta
           name="description"
           content="Willie makes videos and does other artsty stuff. Check out The Willie Diaries"
         />
         <link rel="icon" href="/favicon.ico" />
 
-        <meta property="og:title" content="Willie Chalmers III" />
+        <meta property='og:site_name' content="Willie Chalmers III" />
+        <meta property="og:title" content="Media and Art Initiatives" />
         {/* <meta property="og:image" content="" /> */}
         <meta property="og:type" content="website" />
       </Head>

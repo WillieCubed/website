94ecde1abb258a71bc847610c351a9094349a342
Willie Chalmers III <willie@williecubed.me>
2022-11-24 00:51:35 -0600

    chore: Overhaul Willie Theme

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -9,25 +9,37 @@ interface SiteHeaderProps {
 
 export default function SiteHeader({ showTitle = true }: SiteHeaderProps) {
   return (
-    <header className="sticky top-0 px-4 lg:px-8 py-2 lg:py-6 bg-white dark:bg-slate-700 shadow-lg z-100">
+    <header className="sticky top-0 px-4 lg:px-8 py-2 lg:py-5 bg-primary-dark-2 text-white border-b-4 border-black">
       <nav className="md:flex container max-w-6xl mx-auto text-center md:text-left justify-center md:justify-left">
-        <div className="flex-grow font-bold font-display text-xl">
-          {showTitle && <Link href="/">Willie Chalmers III</Link>}
+        <div className="flex-grow font-bold py-2 lg:py-0 font-display text-2xl">
+          {showTitle && (
+            <Link
+              href="/"
+              className="hover:underline focus:underline underline-offset-4"
+            >
+              Willie Chalmers III
+            </Link>
+          )}
         </div>
-        <ol className="flex space-x-4">
+        <ul className="flex space-x-4 py-2 lg:py-0">
           {/* <li>
             <Link href="/research">Research</Link>
           </li>
           <li>
             <Link href="/projects">Projects</Link>
           </li> */}
-          <li className="font-bold text-md uppercase">
+          {/* <li className="font-bold font-display text-2xl">
             <Link href="/media">Media</Link>
+          </li> */}
+          <li className="font-bold font-display text-2xl">
+            <Link
+              href="/resume.pdf"
+              className="hover:underline focus:underline underline-offset-4"
+            >
+              Resume
+            </Link>
           </li>
-          <li className="font-bold text-md uppercase">
-            <Link href="/resume.pdf">Resume</Link>
-          </li>
-        </ol>
+        </ul>
       </nav>
     </header>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,64 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        "sans": ['Roboto', 'system-ui'],
-        'display': ['Work Sans', 'ui-sans-serif'],
+        sans: ['Roboto', 'system-ui'],
+        display: ['Work Sans', 'ui-sans-serif'],
+      },
+      fontSize: {
+        'display-large': [
+          '48px',
+          {
+            lineHeight: '56px',
+            letterSpacing: '0.025em',
+            fontWeight: '700',
+          },
+        ],
+        'display-medium': [
+          '34px',
+          {
+            lineHeight: '40px',
+            fontWeight: '600',
+          },
+        ],
+        'display-small': [
+          '24px',
+          {
+            lineHeight: '28px',
+            fontWeight: '700',
+          },
+        ],
       },
       colors: {
-        'primary': '#3C84FC',
-        'secondary': '#00FCA8',
-        'accent': '#7284FF',
-        'important': '#FC3C82',
+        primary: {
+          'light-1': '#5E98FD',
+          DEFAULT: '#3C84FC',
+          'dark-1': '#0E65FB',
+          'dark-2': '#023CA1',
+        },
+        secondary: {
+          'light-1': '#33FFBB',
+          DEFAULT: '#00FCA8',
+          'dark-1': '#00E096',
+          'dark-2': '#00CC88',
+        },
+        accent: '#7284FF',
+        important: '#FC3C82',
         'on-primary': '#F9F9F9',
         'on-secondary': '#404040',
         'on-dark': '#FAFAFA',
-        'dark': '#2A2A2A',
-        'light': {
-          '1': '#F5F5F5',
+        dark: '#2A2A2A',
+        light: {
+          1: '#F5F5F5',
           DEFAULT: '#FAFAFA',
         },
-        'link': '#3324BB',
+        link: '#3324BB',
       },
     },
   },
   plugins: [],
-}
+};

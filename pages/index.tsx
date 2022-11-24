0ca68a95c3af01814599635859673ad244a4eef6
Willie Chalmers III <willie@williecubed.me>
2022-11-24 01:28:22 -0600

    chore: Update meta tags for index page
    Now there's a more meaningful description and a canonical URL for the
    page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,13 +18,15 @@ const LandingPage: NextPage = () => {
         <title>Willie Chalmers III</title>
         <meta
           name="description"
-          content="Everything WillieCubed. See what projects he's working on."
+          content="Willie Chalmers III is a student studying computer science at The University of Texas at Dallas and doing research in artficial intelligence. See what he's working on here."
         />
         <link rel="icon" href="/favicon.ico" />
 
         <meta property="og:title" content="Willie Chalmers III" />
+        <meta property="og:description" content="Willie Chalmers III is a student studying computer science at The University of Texas at Dallas and doing research in artficial intelligence. See what he's working on here." />
         <meta property="og:image" content="/assets/headshot.jpg" />
         <meta property="og:type" content="website" />
+        <meta property='og:url' content='https://williecubed.me' />
       </Head>
 
       <SiteHeader />

e73795d3f8c3406d1950d3ed58b10b5e9d42af9d
Willie Chalmers III <willie@williecubed.me>
2022-12-27 21:33:49 -0800

    perf: Enable preloading for fonts
    See https://nextjs.org/docs/messages/google-fonts-missing-subsets

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
+    fontLoaders: [
+      { loader: '@next/font/google', options: { subsets: ['latin'] } },
+    ],
+
   },
 };
 

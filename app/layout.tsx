90380f3fa8f6d1464392cc10651dd471b6fe0ad2
Willie Chalmers III <willie@williecubed.me>
2022-12-29 16:09:07 -0800

    fix: Fix dev server not showing proper fonts
    Apparently the dev server is a little slow, and Next.js won't swap
    fonts if it takes them more than 100 ms to load.
    See this for more info: https://github.com/vercel/next.js/discussions/38710#discussioncomment-4426448
    
    TODO: Change back to default or figure out a better solution

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,22 +6,26 @@ import './globals.css';
 const sansFont = Roboto({
   weight: ['500', '700'],
   variable: '--font-sans',
+  display: 'auto',
 });
 
 const displayFont = Work_Sans({
   weight: ['500', '600', '700'],
   variable: '--font-display',
+  display: 'auto',
 });
 
 export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <html lang="en" className={`${sansFont.variable} ${displayFont.variable}`}>
       <head />
-      <body className={"min-h-screen bg-light text-on-light dark:bg-slate-900 dark:text-on-dark"}>
-        <GeneralLayout>
-          {children}
-        </GeneralLayout>
+      <body
+        className={
+          'min-h-screen bg-light text-on-light dark:bg-slate-900 dark:text-on-dark'
+        }
+      >
+        <GeneralLayout>{children}</GeneralLayout>
       </body>
-    </html >
+    </html>
   );
 }

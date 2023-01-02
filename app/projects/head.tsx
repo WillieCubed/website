fc0cea7a8511256855a8dfcc1ae47a634f36e850
Willie Chalmers III <willie@williecubed.me>
2023-01-01 23:58:43 -0800

    feat: Add useful meta information to projects pages
    This is a small quality of life change.

--- app/projects/head.tsx
+++ app/projects/head.tsx
@@ -1,10 +1,14 @@
-export default function Head({ params }: { params: {} }) {
+import { getProjects } from '../../lib/projects';
+
+export default async function Head({}: { params: {} }) {
+  const projects = await getProjects();
+  const count = projects.length;
   return (
     <>
       <title>Projects Overview - Willie Chalmers III</title>
       <meta
         name="description"
-        content="Everything WillieCubed. See what projects he's working on."
+        content={`Willie Chalmers III is a student studying computer science at The University of Texas at Dallas and doing research in artficial intelligence. See his ${count} projects here.`}
       />
       <link rel="icon" href="/favicon.ico" />
 
@@ -12,7 +16,7 @@ export default function Head({ params }: { params: {} }) {
       <meta property="og:title" content="Projects Overview" />
       <meta
         property="og:description"
-        content="Willie Chalmers III is a student studying computer science at The University of Texas at Dallas and doing research in artficial intelligence. See what he's working on here."
+        content={`Willie Chalmers III is a student studying computer science at The University of Texas at Dallas and doing research in artficial intelligence. See his ${count} projects here.`}
       />
       <meta property="og:image" content="/assets/headshot.jpg" />
       <meta property="og:type" content="website" />

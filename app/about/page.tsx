8b0436cb2fbcc19150068575a296fcadbd460196
Willie Chalmers III <willie@williecubed.me>
2023-01-02 00:33:11 -0800

    chore: Start work on about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,28 @@
+/**
+ * An overview of who I am and what I believe along with a small autobiography.
+ *
+ * Route: /about
+ */
 export default function AboutPage() {
-  return <div>Coming soon!</div>;
+  return (
+    <main>
+      <section id="overview"></section>
+      <BigWordsBox>
+        People and teams matter far more than tools and technologies.
+      </BigWordsBox>
+      <BigWordsBox>
+        The future belongs to the people who build it, for better or for worse.
+      </BigWordsBox>
+    </main>
+  );
+}
+
+function BigWordsBox({ children }: React.PropsWithChildren) {
+  return (
+    <section className="h-[60vh] py-16 flex">
+      <blockquote className="my-auto max-w-6xl ml-48 p-16 text-[72px] font-display font-semibold bg-[#ffffff22]">
+        {children}
+      </blockquote>
+    </section>
+  );
 }

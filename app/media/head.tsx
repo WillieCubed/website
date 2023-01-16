f6fb9b56775731b144dfbf134514018e1076829a
Willie Chalmers III <willie@williecubed.me>
2023-01-16 13:30:30 -0600

    feat: Add meta descriptions to media page

--- app/media/head.tsx
+++ app/media/head.tsx
@@ -4,12 +4,16 @@ export default function Head() {
       <title>Media and Art Initiatives - Willie Chalmers III</title>
       <meta
         name="description"
-        content="Willie makes videos and does other artsty stuff. Check out The Willie Diaries"
+        content={`Outside of doing research and developing software, Willie makes videos and does other creative stuff. Check out The Willie Diaries and more here.`}
       />
       <link rel="icon" href="/favicon.ico" />
 
       <meta property="og:site_name" content="Willie Chalmers III" />
       <meta property="og:title" content="Media and Art Initiatives" />
+      <meta
+        property="og:description"
+        content={`Outside of doing research and developing software, Willie makes videos and does other creative stuff. Check out The Willie Diaries and more here.`}
+      />
       {/* <meta property="og:image" content="" /> */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://williecubed.me/media" />

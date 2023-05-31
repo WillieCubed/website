cfe8cf196f9c8287d49d711e6073ff548afd5632
Willie Chalmers III <willie@williecubed.me>
2023-05-30 22:55:28 -0500

    fix: Fix header visibility in dark mode
    And tweak the background contrast

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -12,7 +12,7 @@ interface SiteHeaderProps {
 export default function SiteHeader({ showTitle = true }: SiteHeaderProps) {
   return (
     <header className="mx-auto container sticky top-0 z-50 bg-light-1 dark:bg-slate-900">
-      <nav className="sticky top-4 md:flex mx-auto px-4 lg:mt-4 lg:pb-4 pb-2 lg:py-4 text-center md:text-left justify-center md:justify-left bg-white border-4 border-black ">
+      <nav className="sticky top-4 md:flex mx-auto px-4 lg:my-4 lg:pb-4 pb-2 lg:py-4 text-center md:text-left justify-center md:justify-left border-4 border-black ">
         <div className="flex-grow font-bold py-2 lg:py-0 font-display text-xl">
           {showTitle && (
             <Link

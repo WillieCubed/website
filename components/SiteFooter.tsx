1b771cfe722f8da717d8cb517d1af987b0a76e33
Willie Chalmers III <willie@williecubed.me>
2023-10-02 11:58:53 -0700

    feat: Redesign site footer

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import taglines from '../lib/tagline_data.json';
+import Link from 'next/link';
 
 /**
  * Generate a random tagline quote.
@@ -23,66 +24,160 @@ export default function SiteFooter() {
   }, []);
 
   return (
-    <footer className="bg-primary-dark-1 text-on-dark dark:bg-slate-900 dark:text-slate-300 dark:border-t-2 dark:border-slate-500/50">
-      <div className="mx-auto max-w-6xl lg:grid lg:gap-4 pt-4 pb-8 px-4 lg:grid-cols-4">
+    <footer className="bg-white text-on-light dark:bg-slate-900 dark:text-slate-300 border-t-2 border-black dark:border-slate-500/50">
+      <div className="space-y-4 lg:space-y-0 md:grid lg:grid-cols-12 md:gap-4 lg:gap-6 py-16 px-4">
         <div
-          id="quote"
-          className="mt-2 mb-4 md:px-4 text-xl text-center md:text-left font-display lg:col-span-2"
+          id="tagline"
+          className="text-headline-small pb-12 md:mb-0 lg:text-headline-medium text-center lg:text-left font-display row-start-1 col-start-1 lg:col-start-2 md:col-span-3"
         >
           {footerTagline}
         </div>
-        <div>{/* TODO: Put other personal tools here */}</div>
-        <div>
-          <div className="my-2 font-bold font-display text-md dark:text-white">
-            Find me elsewhere
+        <div className="row-start-2 md:row-start-1 lg:col-span-2 lg:col-start-6">
+          <div className="text-title-large font-display">My Work</div>
+          <div className="mt-4">
+            <ul className="space-y-2">
+              <li>
+                <Link
+                  href="/projects"
+                  className="font-display text-title-small dark:text-white opacity-[0.87] hover:opacity-100 dark:hover:text-white"
+                >
+                  Technical Projects
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/design"
+                  className="font-display text-title-small dark:text-white opacity-[0.87] hover:opacity-100 dark:hover:text-white"
+                >
+                  Design
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/research"
+                  className="font-display text-title-small dark:text-white opacity-[0.87] hover:opacity-100 dark:hover:text-white"
+                >
+                  Research
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/media"
+                  className="font-display text-title-small dark:text-white opacity-[0.87] hover:opacity-100 dark:hover:text-white"
+                >
+                  Media
+                </Link>
+              </li>
+            </ul>
           </div>
-          <ul>
-            <li>
-              <a
-                className="font-normal text-white opacity-[0.87] hover:opacity-100 hover:text-white"
-                href="https://github.com/WillieCubed"
-              >
-                GitHub (@WillieCubed)
-              </a>
-            </li>
-            <li>
-              <a
-                className="font-normal text-white opacity-[0.87] hover:opacity-100 hover:text-white"
-                href="https://twitter.com/WillieCubed"
-              >
-                Twitter (@WillieCubed)
-              </a>
-            </li>
-            <li>
-              <a
-                className="font-normal text-white opacity-[0.87] hover:opacity-100 hover:text-white"
-                href="https://sigmoid.social/@willie"
-                rel="me"
-              >
-                Mastodon (@willie@sigmoid.social)
-              </a>
-            </li>
-            <li className="hover:text-emerald-400">
-              <a
-                className="font-normal text-white opacity-[0.87] hover:opacity-100 hover:text-white"
-                href="https://instagram.com/williecubed"
-              >
-                Instagram (@williecubed)
-              </a>
-            </li>
-            <li className="hover:text-emerald-400">
-              <a
-                className="font-normal text-white opacity-[0.87] hover:opacity-100 hover:text-white"
-                href="https://linked.com/in/willie-chalmers-iii"
-              >
-                LinkedIn (Willie Chalmers III)
-              </a>
-            </li>
-          </ul>
+        </div>
+        <div className="row-start-2 md:row-start-1 lg:col-span-2">
+          <div className="text-title-large font-display">About Me</div>
+          <div className="mt-4">
+            <ul className="space-y-2">
+              <li>
+                <Link
+                  href="/now"
+                  className="font-display text-title-small dark:text-white opacity-[0.87] hover:opacity-100 dark:hover:text-white"
+                >
+                  Current stuff
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/resume.pdf"
+                  className="font-display text-title-small dark:text-white opacity-[0.87] hover:opacity-100 dark:hover:text-white"
+                >
+                  Resume
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/bio"
+                  className="font-display text-title-small dark:text-white opacity-[0.87] hover:opacity-100 dark:hover:text-white"
+                >
+                  Bio
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://blog.williecubed.me"
+                  className="font-display text-title-small dark:text-white opacity-[0.87] hover:opacity-100 dark:hover:text-white"
+                >
+                  Blog
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="row-start-2 md:row-start-1 lg:col-span-2">
+          <div className="text-title-large font-display">Find me elsewhere</div>
+          <div className="mt-4">
+            <ul className="space-y-2">
+              <li>
+                <a
+                  href="https://threads.net/@williecubed"
+                  className="font-display text-title-small dark:text-white opacity-[0.87] hover:opacity-100 dark:hover:text-white"
+                  rel="me"
+                >
+                  Threads
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://sigmoid.social/@willie"
+                  className="font-display text-title-small dark:text-white opacity-[0.87] hover:opacity-100 dark:hover:text-white"
+                  rel="me"
+                >
+                  Mastodon
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://instagram.com/williecubed"
+                  className="font-display text-title-small dark:text-white opacity-[0.87] hover:opacity-100 dark:hover:text-white"
+                >
+                  Instagram
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/WillieCubed"
+                  className="font-display text-title-small dark:text-white opacity-[0.87] hover:opacity-100 dark:hover:text-white"
+                >
+                  GitHub
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/willie-chalmers-iii"
+                  className="font-display text-title-small dark:text-white opacity-[0.87] hover:opacity-100 dark:hover:text-white"
+                >
+                  LinkedIn
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.last.fm/user/WillieCubed"
+                  className="font-display text-title-small dark:text-white opacity-[0.87] hover:opacity-100 dark:hover:text-white"
+                >
+                  Last.fm
+                </a>
+              </li>
+              <li>
+                <a
+                  href="mailto:hello@williecubed.me?subject=Hello%20Willie!"
+                  className="font-display text-title-small dark:text-white opacity-[0.87] hover:opacity-100 dark:hover:text-white"
+                >
+                  Email
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <div
-        className="px-4 py-2 text-center text-sm bg-primary-dark-2 text-on-dark dark:bg-blue-900 dark:text-white cursor-pointer font-display"
+        className="px-4 py-2 text-center text-sm bg-primary-light-1 text-on-primary dark:text-on-dark dark:bg-blue-900 cursor-pointer font-display"
         title="It took nearly half a decade, but it's better than nothing."
       >
         Built with great patience by Willie.

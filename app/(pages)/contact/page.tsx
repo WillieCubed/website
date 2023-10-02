f982643c7398fcbe0455b928d589868a4f984882
Willie Chalmers III <willie@williecubed.me>
2023-10-02 11:58:09 -0700

    feat: Create cool contact page

---
+++ app/(pages)/contact/page.tsx
@@ -0,0 +1,96 @@
+import { Metadata } from 'next';
+import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: 'Contact',
+  description: "See how to get in touch with me. It's pretty simple.",
+  openGraph: {
+    title: 'Contact',
+    description: "See how to get in touch with me. It's pretty simple.",
+    url: '/contact',
+    type: 'website',
+    images: ['/assets/headshot.jpg'],
+  },
+};
+
+export default function ContactPage() {
+  return (
+    <div className="">
+      <section className="-mt-[100px] pt-40 md:grid md:grid-cols-12 p-16 xl:px-24 bg-maverick-300">
+        <div className="lg:col-start-3 col-span-4 space-y-8">
+          <div className="text-display-medium">Hey there!</div>
+          <div className="text-headline-small">
+            You're probably looking for something.
+            <br />
+            Let me help.
+          </div>
+        </div>
+      </section>
+      <main className="p-16 md:grid md:grid-cols-12 xl:px-24 md:gap-x-4">
+        <section className="md:col-start-3 md:col-span-4 space-y-6">
+          <div className="space-y-4">
+            <h1 className="text-title-large">Want to talk to me online?</h1>
+            <p className="text-body-medium">
+              Find me on your preferred social media platform. I&apos;ll likely
+              respond within a few hours.
+            </p>
+          </div>
+          <div className="space-y-4">
+            <h1 className="text-title-large">
+              Reaching out with an opportunity?
+            </h1>
+            <p className="text-body-medium">
+              Send me a message on{' '}
+              <Link href="https://www.linkedin.com/in/willie-chalmers-iii">
+                LinkedIn
+              </Link>{' '}
+              or <Link href="mailto:hello@williecubed.me">email me</Link>.
+            </p>
+          </div>
+          <div className="space-y-4">
+            <h1 className="text-title-large">
+              Want to say hi for some other reason?
+            </h1>
+            <p className="text-body-medium">
+              Just send an email to hello@williecubed.me. I&apos;ll get back to
+              you within a day.
+            </p>
+          </div>
+        </section>
+        <section id="social-media" className="md:col-span-4 space-y-6">
+          <dl className="inline-block">
+            <dd className="text-title-large">Instagram</dd>
+            <dt className="mt-2 text-headline-small">
+              <Link href="https://instagram.com/williecubed">@williecubed</Link>
+            </dt>
+            <dd className="mt-4 text-title-large">Threads</dd>
+            <dt className="mt-2 text-headline-small">
+              <Link href="https://threads.net/williecubed">
+                @williecubed@threads.net
+              </Link>
+            </dt>
+          </dl>
+          <div>
+            <div className="text-title-large">Email</div>
+            <div className="mt-2">
+              <Link
+                className="text-headline-small"
+                href="mailto:hello@williecubed.me"
+              >
+                hello@williecubed.me
+              </Link>
+            </div>
+            <div className="mt-2">
+              <Link
+                className="text-headline-small"
+                href="mailto:projects@williecubed.me"
+              >
+                projects@williecubed.me
+              </Link>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}

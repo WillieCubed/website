c56bb4357d9e5dcdd765826f98954d0d802557cd
Willie Chalmers III <willie@williecubed.me>
2023-10-02 13:26:51 -0700

    chore: Add research project page scaffold

---
+++ app/(pages)/research/[projectId]/page.tsx
@@ -0,0 +1,20 @@
+type ResearchProject = {
+  title: string;
+  description: string;
+};
+
+/**
+ * A page showing off a single research project.
+ * 
+ * Route: /research/[projectId]
+ */
+export default function ResearchDetailPage() {
+  return (
+    <div className="max-w-4xl mx-auto text-center">
+      Coming soon.
+      {/* <div className="md:grid md:grid-cols-4 lg:grid-cols-8 xl:grid-cols-12">
+
+      </div> */}
+    </div>
+  );
+}

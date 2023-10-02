5aa3dd7d45407f8a7a91bfa557ea27c5417bef99
Willie Chalmers III <willie@williecubed.me>
2023-10-02 12:31:15 -0700

    feat: Add bio page placeholder

---
+++ app/(pages)/bio/page.tsx
@@ -0,0 +1,11 @@
+import { redirect } from 'next/navigation';
+
+/**
+ * A page with a more detailed biography of me.
+ * 
+ * Route: /bio
+ */
+export default function BioPage() {
+  // TODO: Actually write a bio
+  redirect('/about');
+}

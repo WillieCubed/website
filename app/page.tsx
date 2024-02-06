663157ea47f53798c24d5305a4cfccc4e9d9a82a
Willie Chalmers III <willie@williecubed.me>
2024-02-05 21:08:57 -0800

    fix: Add missing landing page updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
+import { get } from '@vercel/edge-config';
 import Link from 'next/link';
+import Image from 'next/image';
 import type { Metadata } from 'next/types';
-import SiteFooter from '../components/SiteFooter';
+import './landing.css';
+import nowIcon from '@/assets/list_alt.svg';
+import bioIcon from '@/assets/patient_list.svg';
+import randomIcon from '@/assets/draw_collage.svg';
+import { SocialContactChip } from '../components/landing/SocialContactChip';
+import { getFeaturedProjects } from '../lib/projects';
+import FeaturedWorkCard from '../components/FeaturedProject';
+import FeaturedProjectsList from '../components/landing/FeaturedProjectsList';
+import FeaturedProjectsIcon from '../components/icons/FeaturedProjectsIcon';
+import FeaturedWorkIcon from '../components/icons/FeaturedWorkIcon';
+import InstagramIcon from '../components/icons/InstagramIcon';
+import LinkedinIcon from '../components/icons/LinkedinIcon';
+import NotesIcon from '../components/icons/NotesIcon';
+import ThreadsIcon from '../components/icons/ThreadsIcon';
+import { LinkButton } from '../components/LinkButton';
+import { getFeaturedWork } from '../lib/projects';
 
 export const metadata: Metadata = {
   title: {
@@ -22,88 +39,175 @@ export const metadata: Metadata = {
  *
  * Route: /
  */
-export default function LandingPage() {
+export default async function LandingPage() {
+  const featuredProjects = await getFeaturedProjects();
+  const featuredWork = await getFeaturedWork();
+  const shouldShowWritings = await get('show_writings');
+
   return (
-    <>
-      <main className="min-h-[90vh] p-4 lg:p-6 flex flex-col">
-        <div className="h-full md:grid md:grid-cols-12 md:gap-x-4">
-          <section className="md:col-start-1 md:col-end-10 lg:col-start-2 xl:col-end-12">
-            <div className="mt-[108px] space-y-4">
+    <div className="p-lg pb-2xl desktop:pb-[128px]">
+      <main className="max-w-breakpoint-2xl mx-auto tablet:grid tablet:grid-cols-8 tablet:gap-lg">
+        <div className="hero-fold h-full tablet:col-span-5">
+          <section className="md:px-4 md:col-start-1 md:col-end-10 lg:col-start-3 xl:col-end-11">
+            <div className="mt-[96px] space-y-4">
               <div className="text-display-medium font-display">Hello!</div>
               <div className="text-display-medium font-display">
-                I&apos;m Willie, an aspiring AI researcher.
+                I&apos;m <span className="text-primary">Willie</span>.
               </div>
             </div>
-            <div className="mt-8 lg:row-start-2 lg:col-span-5">
-              <div className="text-display-small font-display">
-                I{' '}
+            <div className="mt-6 space-y-4">
+              <div className="text-headline-large font-display">
+                I build{' '}
                 <span
                   className="text-primary hover:underline cursor-pointer"
+                  title="(create software that uses AI)"
+                >
+                  thoughtful software
+                </span>{' '}
+                to bring people together
+                <br className="print:hidden" /> and aspire to{' '}
+                <span
+                  className="text-accent-dark-2 hover:underline cursor-pointer"
                   title="(search for the holy grail of artificial general intelligence)"
                 >
                   study machine intelligence
                 </span>
-                <br />
-                and{' '}
-                <span
-                  className="text-accent-dark-2 hover:underline cursor-pointer"
-                  title="(create software that uses AI)"
-                >
-                  build tools
-                </span>{' '}
-                that bring people together.
+                .
+              </div>
+              <div className="max-w-[598px]">
+                <div className="text-headline-small">
+                  Currently a product manager for{' '}
+                  <a
+                    href="https://www.developforgood.org/"
+                    className="text-maverick-400 hover:text-maverick-500 transition ease-linear underline"
+                  >
+                    Develop for Good
+                  </a>{' '}
+                  working with the{' '}
+                  <a
+                    href="https://www.asaging.org/"
+                    className="text-maverick-400 hover:text-maverick-500 transition ease-linear underline"
+                  >
+                    American Society on Aging
+                  </a>
+                  .
+                </div>
               </div>
             </div>
           </section>
-          <section
-            className="mt-8 lg:mt-24 md:col-start-6 md:col-end-12 lg:row-start-2 md:row-start-2 "
-            id="links"
-          >
-            <h1 className="text-title-large font-bold font-display">My Work</h1>
-            <div className="md:flex w-full mt-2 space-y-4 md:space-y-0 md:space-x-4">
-              <Link
-                href="/research"
-                className="block min-h-[240px] flex-1 p-4 border-2 fancy-shadow border-black bg-primary text-white hover:text-neutral-200 content-end"
-              >
-                {/* The hover:text-white is a temporary hack because of the global.css link styles */}
-                <div className="font-display text-headline-small">Research</div>
-                <div className="mt-2 font-display">Learn more</div>
-              </Link>
-              <Link
-                href="/projects"
-                className="block min-h-[240px] flex-1 p-4 border-2 fancy-shadow border-black bg-accent text-white hover:text-neutral-200"
-              >
-                <div className="font-display text-headline-small">
-                  Personal Projects
-                </div>
-                <div className="mt-2 font-display">Learn more</div>
-              </Link>
+          <section className="mt-8 lg:mt-24 md:col-span-5" id="spotlight">
+            <div className="flex px-3 py-4 items-center gap-x-3 self-stretch">
+              <FeaturedWorkIcon />
+              <h1 className="text-headline-small font-bold font-display">
+                Current Work
+              </h1>
             </div>
+            <FeaturedWorkCard
+              title={featuredWork.title}
+              tagline={featuredWork.tagline}
+              description={featuredWork.description}
+              timePeriod={featuredWork.timePeriod}
+              mainLink={`https://github.com/ConnieML/Connie-RTC/`}
+              caseStudyLink={`/case-studies/${featuredWork.projectId}`}
+              type={featuredWork.type}
+            />
           </section>
-          <section className="bottom-0 inset-0 md:sticky md:top-24 md:mt-24 md:col-start-1 lg:mt-24 lg:col-start-2 md:col-span-4 md:row-start-2">
-            <div className="mt-4">
+        </div>
+        <div className="hero-fold h-full tablet:pb-lg tablet:col-start-7 tablet:col-end-9 tablet:row-start-1">
+          <section
+            id="links"
+            className="h-full flex flex-col justify-end bottom-0 inset-0 tablet:sticky tablet:top-24 pb-4 mt-xl space-y-8"
+          >
+            <div>
               <h1 className="font-semibold font-display text-title-large">
                 Quick links
               </h1>
-              <ul className="mt-2 text-title-medium">
-                <li>
-                  <Link href="/now">See what I&apos;m working on</Link>
+              <ul className="mt-2 flex flex-col items-start text-title-medium">
+                <li className="text-on-surface">
+                  <Link
+                    href="/now"
+                    className="flex items-center h-10 gap-x-2 group rounded-lg group-hover:bg-slate-300 icon-link"
+                  >
+                    <Image
+                      src={nowIcon}
+                      alt=""
+                      width={24}
+                      height={24}
+                      className="group-hover:"
+                    />
+                    <span>See what I&apos;m working on</span>
+                  </Link>
                 </li>
-                <li>
-                  <Link href="/about">Learn more about me</Link>
+                <li className="text-on-surface">
+                  <Link href="/bio" className="flex items-center h-10 gap-x-2">
+                    <Image src={bioIcon} alt="" width={24} height={24} />
+                    <span>Learn more about me</span>
+                  </Link>
                 </li>
-                <li>
-                  <Link href="/random">See something random</Link>
-                </li>
-                <li>
-                  <Link href="/contact">Contact me</Link>
+                <li className="text-on-surface">
+                  <Link
+                    href="/random"
+                    className="flex items-center h-10 gap-x-2"
+                  >
+                    <Image src={randomIcon} alt="" width={24} height={24} />
+                    <span>See something random</span>
+                  </Link>
                 </li>
               </ul>
             </div>
+            <div className="flex flex-col space-y-lg">
+              <h1 className="font-semibold font-display text-title-large">
+                Quick contacts
+              </h1>
+              <div className="flex items-end flex-wrap space-r-lg space-y-sm">
+                <SocialContactChip
+                  href="https://threads.net/@williecubed"
+                  icon={
+                    <ThreadsIcon className="group-hover:stroke-primary text-on-surface-foreground" />
+                  }
+                  label="Threads"
+                />
+                <SocialContactChip
+                  href="https://instagram.com/williecubed"
+                  icon={
+                    <InstagramIcon className="group-hover:stroke-primary text-on-surface-foreground" />
+                  }
+                  label="Instagram"
+                />
+                <SocialContactChip
+                  href="https://www.linkedin.com/in/willie-chalmers-iii"
+                  icon={
+                    <LinkedinIcon className="group-hover:stroke-primary text-on-surface-foreground" />
+                  }
+                  label="LinkedIn"
+                />
+              </div>
+            </div>
           </section>
         </div>
+        <section id="projects" className="md:col-span-5">
+          <div className="flex px-4 py-4 items-center gap-x-3 self-stretch">
+            <FeaturedProjectsIcon />
+            <h1 className="text-headline-small font-bold font-display">
+              Selected Projects
+            </h1>
+          </div>
+          <div className="space-y-md">
+            <FeaturedProjectsList projects={featuredProjects} />
+            <LinkButton href={''} label="See the rest" icon={<></>} />
+          </div>
+        </section>
+        {shouldShowWritings && (
+          <section id="writings" className="md:col-span-5">
+            <div className="flex px-4 py-4 items-center gap-x-3 self-stretch">
+              <NotesIcon />
+              <h1 className="text-headline-small font-bold font-display">
+                Some Writings
+              </h1>
+            </div>
+          </section>
+        )}
       </main>
-      <SiteFooter />
-    </>
+    </div>
   );
 }

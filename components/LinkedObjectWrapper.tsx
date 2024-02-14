1617cf86f89210860557fe349d2699b3c86f1c62
Willie Chalmers III <willie@williecubed.me>
2024-02-13 23:26:59 -0800

    chore: Allow LinkedObjectWrapper class customization

--- components/LinkedObjectWrapper.tsx
+++ components/LinkedObjectWrapper.tsx
@@ -1,14 +1,17 @@
+import clsx from 'clsx';
 import Link from 'next/link';
 import { PropsWithChildren } from 'react';
 
 interface LinkedObjectWrapperProps {
   href: string;
   openInNewTab?: boolean;
+  className?: string;
 }
 
 export default function LinkedObjectWrapper({
   href,
   openInNewTab,
+  className,
   children,
 }: PropsWithChildren<LinkedObjectWrapperProps>) {
   return (
@@ -16,7 +19,10 @@ export default function LinkedObjectWrapper({
       href={href}
       target={openInNewTab ? '_blank' : undefined}
       rel={openInNewTab ? 'noopener noreferrer' : undefined}
-      className="inline-flex flex-col items-start gap-x-md px-lg py-sm bordered bg-maverick-300 hover:bg-maverick-400 focus:bg-maverick-400 dark:bg-maverick-800 hover:dark:bg-maverick-700 focus:dark:bg-maverick-700 transition ease-in duration-100 "
+      className={clsx(
+        'inline-flex flex-col items-start gap-x-md px-lg py-sm bordered bg-maverick-300 hover:bg-maverick-400 focus:bg-maverick-400 dark:bg-maverick-800 hover:dark:bg-maverick-700 focus:dark:bg-maverick-700 transition ease-in duration-100',
+        className
+      )}
     >
       {children}
     </Link>

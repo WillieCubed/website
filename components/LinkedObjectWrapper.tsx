ff0735403a4a56acbeffccc3bfedb1ff1c66e3a8
Willie Chalmers III <willie@williecubed.me>
2024-02-14 00:24:24 -0800

    chore: Tweak LinkedObjectWrapper styles

--- components/LinkedObjectWrapper.tsx
+++ components/LinkedObjectWrapper.tsx
@@ -20,7 +20,7 @@ export default function LinkedObjectWrapper({
       target={openInNewTab ? '_blank' : undefined}
       rel={openInNewTab ? 'noopener noreferrer' : undefined}
       className={clsx(
-        'inline-flex flex-col items-start gap-x-md px-lg py-sm bordered bg-maverick-300 hover:bg-maverick-400 focus:bg-maverick-400 dark:bg-maverick-800 hover:dark:bg-maverick-700 focus:dark:bg-maverick-700 transition ease-in duration-100',
+        'inline-flex flex-col items-start gap-x-md px-lg py-sm bordered bg-maverick-300 hover:bg-maverick-400 focus:bg-maverick-400 dark:bg-maverick-800 hover:dark:bg-maverick-700 focus:dark:bg-maverick-700 transition ease-out duration-150',
         className
       )}
     >
